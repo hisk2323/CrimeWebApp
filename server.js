ae2558a01a13a06d8bf903d60cd4444f22b20dc1
Neshua <60229591+Neshua@users.noreply.github.com>
2022-11-27 14:35:33 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,121 @@
+// Built-in Node.js modules
+let fs = require('fs');
+let path = require('path');
+
+// NPM modules
+let express = require('express');
+let sqlite3 = require('sqlite3');
+
+
+let db_filename = path.join(__dirname, 'db', 'stpaul_crime.sqlite3');
+
+let app = express();
+let port = 8000;
+
+//Express will automatically parse the data to a json object
+app.use(express.json());
+
+// Open SQLite3 database (in read-write mode)
+let db = new sqlite3.Database(db_filename, sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.log('Error opening ' + path.basename(db_filename));
+    }
+    else {
+        console.log('Now connected to ' + path.basename(db_filename));
+    }
+});
+
+
+// GET request handler for crime codes
+app.get('/codes', (req, res) => {
+    console.log(req.query); // query object (key-value pairs after the ? in the url)
+    let query = 'SELECT * FROM Codes ORDER BY code'
+    db.all(query, (err, rows) =>{
+        console.log(err)
+        console.log(rows.length)
+        res
+            .status(200)
+            .type('json')
+            .send(rows)
+
+    })
+});
+
+// GET request handler for neighborhoods
+app.get('/neighborhoods', (req, res) => {
+    console.log(req.query); // query object (key-value pairs after the ? in the url)
+    let query = 'SELECT Neighborhoods.neighborhood_number AS id, Neighborhoods.neighborhood_name AS name \
+                FROM Neighborhoods ORDER BY id'
+    db.all(query, (err, rows)=>{
+        console.log(err)
+        console.log(rows.length)
+        res
+            .status(200)
+            .type('json')
+            .send(rows)
+    })
+});
+
+// GET request handler for crime incidents
+app.get('/incidents', (req, res) => {
+    console.log(req.query); // query object (key-value pairs after the ? in the url)
+    let query = "SELECT *, CONVERT(VARCHAR, date_time, 101), CONVERT(VARCHAR, date_time, 108) \
+                FROM Incidents"
+    db.all(query, (err,rows)=>{
+        console.log(err)
+        res
+            .status(200)
+            .type('json')
+            .send(rows)
+    })
+    // res.status(200).type('json').send({}); // <-- you will need to change this
+});
+
+// PUT request handler for new crime incident
+app.put('/new-incident', (req, res) => {
+    console.log(req.body); // uploaded data by user, should be in json format
+    
+    res.status(200).type('txt').send('OK'); // <-- you may need to change this
+});
+
+// DELETE request handler for new crime incident
+app.delete('/remove-incident', (req, res) => {
+    console.log(req.body); // uploaded data
+    
+    res.status(200).type('txt').send('OK'); // <-- you may need to change this
+});
+
+
+// Create Promise for SQLite3 database SELECT query 
+function databaseSelect(query, params) {
+    return new Promise((resolve, reject) => {
+        db.all(query, params, (err, rows) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(rows);
+            }
+        });
+    })
+}
+
+// Create Promise for SQLite3 database INSERT or DELETE query
+function databaseRun(query, params) {
+    return new Promise((resolve, reject) => {
+        db.run(query, params, (err) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve();
+            }
+        });
+    })
+}
+
+
+// Start server - listen for client connections
+app.listen(port, () => {
+    console.log('Now listening on port ' + port);
+});
