3ac9dc1363d303cd57916c8dc9c0383335b53cf5
Owen <o.hisk@icloud.com>
2022-12-12 11:35:56 -0600

    Start working on new features

--- Project4-Vue-dev/src/App.vue
+++ Project4-Vue-dev/src/App.vue
@@ -1,5 +1,6 @@
 <script>
 import $ from 'jquery'
+import { setBlockTracking } from 'vue';
 
 export default {
     data() {
@@ -17,27 +18,27 @@ export default {
                 },
                 zoom: 12,
                 bounds: {
-                    nw: {lat: 45.008206, lng: -93.217977},
-                    se: {lat: 44.883658, lng: -92.993787}
+                    nw: { lat: 45.008206, lng: -93.217977 },
+                    se: { lat: 44.883658, lng: -92.993787 }
                 },
                 neighborhood_markers: [
-                    {location: [44.942068, -93.020521], marker: null},
-                    {location: [44.977413, -93.025156], marker: null},
-                    {location: [44.931244, -93.079578], marker: null},
-                    {location: [44.956192, -93.060189], marker: null},
-                    {location: [44.978883, -93.068163], marker: null},
-                    {location: [44.975766, -93.113887], marker: null},
-                    {location: [44.959639, -93.121271], marker: null},
-                    {location: [44.947700, -93.128505], marker: null},
-                    {location: [44.930276, -93.119911], marker: null},
-                    {location: [44.982752, -93.147910], marker: null},
-                    {location: [44.963631, -93.167548], marker: null},
-                    {location: [44.973971, -93.197965], marker: null},
-                    {location: [44.949043, -93.178261], marker: null},
-                    {location: [44.934848, -93.176736], marker: null},
-                    {location: [44.913106, -93.170779], marker: null},
-                    {location: [44.937705, -93.136997], marker: null},
-                    {location: [44.949203, -93.093739], marker: null}
+                    { location: [44.942068, -93.020521], marker: null },
+                    { location: [44.977413, -93.025156], marker: null },
+                    { location: [44.931244, -93.079578], marker: null },
+                    { location: [44.956192, -93.060189], marker: null },
+                    { location: [44.978883, -93.068163], marker: null },
+                    { location: [44.975766, -93.113887], marker: null },
+                    { location: [44.959639, -93.121271], marker: null },
+                    { location: [44.947700, -93.128505], marker: null },
+                    { location: [44.930276, -93.119911], marker: null },
+                    { location: [44.982752, -93.147910], marker: null },
+                    { location: [44.963631, -93.167548], marker: null },
+                    { location: [44.973971, -93.197965], marker: null },
+                    { location: [44.949043, -93.178261], marker: null },
+                    { location: [44.934848, -93.176736], marker: null },
+                    { location: [44.913106, -93.170779], marker: null },
+                    { location: [44.937705, -93.136997], marker: null },
+                    { location: [44.949203, -93.093739], marker: null }
                 ]
             }
         };
@@ -64,7 +65,7 @@ export default {
                         resolve(response);
                     },
                     error: (status, message) => {
-                        reject({status: status.status, message: status.statusText});
+                        reject({ status: status.status, message: status.statusText });
                     }
                 });
             });
@@ -82,10 +83,20 @@ export default {
                         resolve(response);
                     },
                     error: (status, message) => {
-                        reject({status: status.status, message: status.statusText});
+                        reject({ status: status.status, message: status.statusText });
                     }
                 });
             });
+        },
+        search() {
+            //this.leaflet.map.setView([this.leaflet.center.lat, this.leaflet.center.lng], this.leaflet.zoom);
+            let url = 'https://nominatim.openstreetmap.org/search?' + document.getElementById('search').value;
+            this.getJSON(url).then((result) => {
+                console.log(result);
+            }).catch((err) => {
+                console.log(err)
+            });
+            
         }
     },
     mounted() {
@@ -116,14 +127,40 @@ export default {
     <div class="grid-container">
         <div class="grid-x grid-padding-x">
             <p :class="'cell small-4 ' + ((view === 'map') ? 'selected' : 'unselected')" @click="viewMap">Map</p>
-            <p :class="'cell small-4 ' + ((view === 'new_incident') ? 'selected' : 'unselected')" @click="viewNewIncident">New Incident</p>
+            <p :class="'cell small-4 ' + ((view === 'new_incident') ? 'selected' : 'unselected')"
+                @click="viewNewIncident">New Incident</p>
             <p :class="'cell small-4 ' + ((view === 'about') ? 'selected' : 'unselected')" @click="viewAbout">About</p>
         </div>
     </div>
     <div v-show="view === 'map'">
         <div class="grid-container">
             <div class="grid-x grid-padding-x">
+                <input type="text" id="search" class="cell small-10" placeholder="Search">
+                <button type="button" class="cell small-2 button" @click="search()">Go</button>
+            </div>
+            <div class="grid-x grid-padding-x">
                 <div id="leafletmap" class="cell auto"></div>
+            </div>
+            <div class="grid-x grid-padding-x">
+                <div id="crimetable" class="cell auto">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Case Number</th>
+                                <th>Date</th>
+                                <th>Time</th>
+                                <th>Code</th>
+                                <th>Incident</th>
+                                <th>Police grid</th>
+                                <th>Neighborhood></th>
+                                <th>Block</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
@@ -157,6 +194,7 @@ export default {
     text-align: center;
     cursor: pointer;
 }
+
 .unselected {
     background-color: rgb(200, 200, 200);
     color: black;
